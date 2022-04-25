09c45c1764d66e67c3d199861c5d536a64fd8072
JustDalo <shishlodan@mail.ru>
2022-04-25 14:05:07 +0300

    code refactor

--- src/test/groovy/com/dalo/spring/integrationtests/DatabaseIntegrationTest.groovy
+++ src/test/groovy/com/dalo/spring/integrationtests/DatabaseIntegrationTest.groovy
@@ -23,30 +23,30 @@ class DatabaseIntegrationTest extends Specification {
 
     def "save new user to db"() {
         given:
-        def COUNTRY = new Country(id: 1L, name: "Belarus")
-        def existedUser = new User(id: 1L, firstName: "Danil", lastName: "Shyshla", middleName: "Valerevich", sex: "male", phoneNumber: "+375333450040", email: "@mail.ru", country: COUNTRY)
-        def userToSave = new User(firstName: "Danil", lastName: "Shyshla", middleName: "Valerevich", sex: "male", phoneNumber: "+375333450040", email: "@mail.ru", country: COUNTRY)
-        countryRepository.save(COUNTRY)
+            def COUNTRY = new Country(id: 1L, name: "Belarus")
+            def existedUser = new User(id: 1L, firstName: "Danil", lastName: "Shyshla", middleName: "Valerevich", sex: "male", phoneNumber: "+375333450040", email: "@mail.ru", country: COUNTRY)
+            def userToSave = new User(firstName: "Danil", lastName: "Shyshla", middleName: "Valerevich", sex: "male", phoneNumber: "+375333450040", email: "@mail.ru", country: COUNTRY)
+            countryRepository.save(COUNTRY)
         when:
-        def savedUser = userRepository.save(userToSave)
+            def savedUser = userRepository.save(userToSave)
         then:
-        savedUser == existedUser
+            savedUser == existedUser
     }
 
     def "get all users should return all created users"() {
         when:
-        List<User> users = userRepository.findAll()
+            List<User> users = userRepository.findAll()
         then:
-        users.size() == 1
+            users.size() == 1
     }
 
     def "find user by id should return user with given id"() {
         given:
-        def COUNTRY = new Country(id: 1L, name: "Belarus")
-        def savedUser = new User(id: 1L, firstName: "Danil", lastName: "Shyshla", middleName: "Valerevich", sex: "male", phoneNumber: "+375333450040", email: "@mail.ru", country: COUNTRY)
+            def COUNTRY = new Country(id: 1L, name: "Belarus")
+            def savedUser = new User(id: 1L, firstName: "Danil", lastName: "Shyshla", middleName: "Valerevich", sex: "male", phoneNumber: "+375333450040", email: "@mail.ru", country: COUNTRY)
         when:
-        def user = userRepository.findById(1L)
+            def user = userRepository.findById(1L)
         then:
-        user == Optional.of(savedUser)
+            user == Optional.of(savedUser)
     }
 }
