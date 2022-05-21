7bc8f0140bb89377c8fc74fa22173021d57a72f2
JustDalo <shishlodan@mail.ru>
2022-05-21 09:56:17 +0300

    user and admin authorities added to controller

--- src/main/java/com/dalo/spring/configuration/SecurityConfig.java
+++ src/main/java/com/dalo/spring/configuration/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.dalo.spring.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,14 +13,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(
+    prePostEnabled = true,
+    securedEnabled = true,
+    jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
             .inMemoryAuthentication()
-            .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN")
+            .withUser("admin").password(passwordEncoder().encode("admin")).authorities("ADMIN")
             .and()
-            .withUser("user").password(passwordEncoder().encode("user")).roles("USER");
+            .withUser("user").password(passwordEncoder().encode("user")).authorities("USER");
     }
 
     @Override
--- src/main/java/com/dalo/spring/controller/UserController.java
+++ src/main/java/com/dalo/spring/controller/UserController.java
@@ -10,6 +10,7 @@ import com.dalo.spring.service.IPRequestService;
 import com.dalo.spring.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -30,27 +31,32 @@ public class UserController {
         this.requestService = requestService;
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> getUserById(@PathVariable long id) {
         return new ResponseEntity<UserDto>(userService.getUserById(id), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public ResponseEntity<List<UserDto>> getAllUsers() {
         return new ResponseEntity<List<UserDto>>(userService.getAllUsers(), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
     @PostMapping
     public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto user, HttpServletRequest httpServletRequest) {
         Country country = countryService.getCountryByIP(requestService.getClientIP(httpServletRequest));
         return new ResponseEntity<UserDto>(userService.createUser(user, country), HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
     @PutMapping("/{id}")
     public ResponseEntity<UserDto> updateUser(@PathVariable long id, @Valid @RequestBody UserDto user) {
         return new ResponseEntity<UserDto>(userService.updateUser(user, id), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity deleteUserById(@PathVariable long id) {
         userService.deleteUserById(id);
